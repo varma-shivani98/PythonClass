0e40c5ea87340eb92dfb8e079734e73f446e834a
varma-shivani98 <79257889+varma-shivani98@users.noreply.github.com>
2021-05-26 14:38:51 +0530

    My details program
    The program contains all my details.

---
+++ details.py
@@ -0,0 +1,5 @@
+print("Name:Yalala Shivani")
+print("Reg.No:19378031")
+print("Class:M.Sc Bioinformatics 2nd year")
+print("Department:Centre for Bioinformatics")
+print("University:Pondicherry University")
